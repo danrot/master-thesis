81dcda47eb9ca633d6c54f10dcde83d449c83de8
Daniel Rotter <daniel.rotter@massiveart.com>
2014-10-16 21:53:48 +0200

    renamed files and added overview image

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-pandoc --standalone title.md introduction.md -o thesis.pdf
+for txt in diagrams/*.txt
+do
+    ditaa $txt
+done
+
+pandoc --standalone --toc --number-sections 00_title.md 01_introduction.md -o thesis.pdf
+rm diagrams/*.png
 
